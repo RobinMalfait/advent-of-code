19f0ad19c77c6464ab905e66ea62bc9599078446
Robin Malfait <malfait.robin@gmail.com>
2022-12-11 17:28:13 +0100

    small refactor
    - Use simple array instead of map
    - Simplify current monkey calculation
    - Simplify generating activity monitor
    - Simplify calculating the least common divisor between all divisible_by values of each monkey

--- src/2022/day-11/part-1.ts
+++ src/2022/day-11/part-1.ts
@@ -1,12 +1,12 @@
 export default function (blob: string, rounds: number = 20, stress_reducer: number = 3) {
-  let monkeys = new Map<number, Monkey>(blob.trim().split('\n\n').map(parseMonkey))
-  let activity_monitor = [...Array(monkeys.size).keys()].fill(0)
-  let common = Array.from(monkeys.values()).reduce((common, current) => lcm(common, current.divisible_by), 1)
+  let monkeys = blob.trim().split('\n\n').map(parseMonkey)
+  let activity_monitor = Array(monkeys.length).fill(0)
+  let common = monkeys.reduce((common, current) => lcm(common, current.divisible_by), 1)
 
-  let current_monkey = 0
+  for (let current_monkey of Array(rounds * monkeys.length).keys()) {
+    current_monkey %= monkeys.length
 
-  for (let _ of Array(rounds * monkeys.size)) {
-    let monkey = monkeys.get(current_monkey)
+    let monkey = monkeys[current_monkey]
     activity_monitor[current_monkey] += monkey.items.length
 
     while (monkey.items.length) {
@@ -18,13 +18,11 @@ export default function (blob: string, rounds: number = 20, stress_reducer: numb
       new_worry_level %= common
 
       if (new_worry_level % monkey.divisible_by === 0) {
-        monkeys.get(monkey.goto_true).items.push(new_worry_level)
+        monkeys[monkey.goto_true].items.push(new_worry_level)
       } else {
-        monkeys.get(monkey.goto_false).items.push(new_worry_level)
+        monkeys[monkey.goto_false].items.push(new_worry_level)
       }
     }
-
-    current_monkey = (current_monkey + 1) % monkeys.size
   }
 
   let [a, b] = activity_monitor.sort((a, z) => z - a)
@@ -32,6 +30,7 @@ export default function (blob: string, rounds: number = 20, stress_reducer: numb
 }
 
 interface Monkey {
+  id: number
   items: number[]
   operation: (old: number) => number
   divisible_by: number
@@ -39,29 +38,27 @@ interface Monkey {
   goto_false: number
 }
 
-function parseMonkey(block: string): [number, Monkey] {
+function parseMonkey(block: string): Monkey {
   let lines = block.split('\n').map((line) => line.trim())
-  return [
-    Number(lines[0].replace('Monkey ', '').replace(':', '')),
-    {
-      items: lines[1].replace('Starting items: ', '').split(', ').map(Number),
-      operation: (function () {
-        let [lhs, op, rhs] = lines[2].replace('Operation: new = ', '').split(' ')
-        if (lhs === 'old' && op === '+' && rhs === 'old') {
-          return (old: number) => old + old
-        } else if (lhs === 'old' && op === '*' && rhs === 'old') {
-          return (old: number) => old * old
-        } else if (lhs === 'old' && op === '+') {
-          return (old: number) => old + Number(rhs)
-        } else if (lhs === 'old' && op === '*') {
-          return (old: number) => old * Number(rhs)
-        }
-      })(),
-      divisible_by: Number(lines[3].replace('Test: divisible by ', '')),
-      goto_true: Number(lines[4].replace('If true: throw to monkey ', '')),
-      goto_false: Number(lines[5].replace('If false: throw to monkey ', '')),
-    },
-  ]
+  return {
+    id: Number(lines[0].replace('Monkey ', '').replace(':', '')),
+    items: lines[1].replace('Starting items: ', '').split(', ').map(Number),
+    operation: (function () {
+      let [lhs, op, rhs] = lines[2].replace('Operation: new = ', '').split(' ')
+      if (lhs === 'old' && op === '+' && rhs === 'old') {
+        return (old: number) => old + old
+      } else if (lhs === 'old' && op === '*' && rhs === 'old') {
+        return (old: number) => old * old
+      } else if (lhs === 'old' && op === '+') {
+        return (old: number) => old + Number(rhs)
+      } else if (lhs === 'old' && op === '*') {
+        return (old: number) => old * Number(rhs)
+      }
+    })(),
+    divisible_by: Number(lines[3].replace('Test: divisible by ', '')),
+    goto_true: Number(lines[4].replace('If true: throw to monkey ', '')),
+    goto_false: Number(lines[5].replace('If false: throw to monkey ', '')),
+  }
 }
 
 function lcm(x: number, y: number): number {
