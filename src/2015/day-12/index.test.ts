54ec28f751d0319edefd0c5a49e97b27bec46a82
Robin Malfait <malfait.robin@gmail.com>
2023-01-06 00:22:37 +0100

    add 2015 — day 12

---
+++ src/2015/day-12/index.test.ts
@@ -0,0 +1,36 @@
+import { promises } from 'fs'
+import { resolve } from 'path'
+
+import part1 from './part-1'
+import part2 from './part-2'
+
+let data = promises.readFile(resolve(process.cwd(), 'data', '2015-12.txt'), 'utf8')
+
+describe('Part 1', () => {
+  it.each([
+    ['[1,2,3]', 6],
+    ['{"a":2,"b":4}', 6],
+    ['[[[3]]]', 3],
+    ['{"a":{"b":4},"c":-1}', 3],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part1(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part1(await data)).toMatchInlineSnapshot(`111754`)
+  })
+})
+
+describe('Part 2', () => {
+  it.each([
+    ['[1,2,3]', 6],
+    ['[1,{"c":"red","b":2},3]', 4],
+    ['{"d":"red","e":[1,2,3,4],"f":5}', 0],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part2(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part2(await data)).toMatchInlineSnapshot(`65402`)
+  })
+})
---
+++ src/2015/day-12/part-1.ts
@@ -0,0 +1,17 @@
+export default function (blob: string) {
+  return sumJSONNumbers(JSON.parse(blob))
+}
+
+function sumJSONNumbers(input: any): number {
+  let sum = 0
+
+  if (typeof input === 'number') {
+    sum += input
+  } else if (Array.isArray(input)) {
+    sum += input.reduce((acc, item) => acc + sumJSONNumbers(item), 0)
+  } else if (typeof input === 'object') {
+    sum += Object.values(input).reduce((acc: number, item) => acc + sumJSONNumbers(item), 0) as number
+  }
+
+  return sum
+}
