54291b6ef5d11e80b170b6ad50f8ace85fe0fc88
Robin Malfait <malfait.robin@gmail.com>
2023-11-11 00:58:28 +0100

    add 2015 — day 13

---
+++ src/2015/day-13/index.test.ts
@@ -0,0 +1,63 @@
+import { promises } from 'fs'
+import { resolve } from 'path'
+
+import part1 from './part-1'
+import part2 from './part-2'
+
+let data = promises.readFile(resolve(process.cwd(), 'data', '2015-13.txt'), 'utf8')
+
+describe('Part 1', () => {
+  it.each([
+    [
+      `
+        Alice would gain 54 happiness units by sitting next to Bob.
+        Alice would lose 79 happiness units by sitting next to Carol.
+        Alice would lose 2 happiness units by sitting next to David.
+        Bob would gain 83 happiness units by sitting next to Alice.
+        Bob would lose 7 happiness units by sitting next to Carol.
+        Bob would lose 63 happiness units by sitting next to David.
+        Carol would lose 62 happiness units by sitting next to Alice.
+        Carol would gain 60 happiness units by sitting next to Bob.
+        Carol would gain 55 happiness units by sitting next to David.
+        David would gain 46 happiness units by sitting next to Alice.
+        David would lose 7 happiness units by sitting next to Bob.
+        David would gain 41 happiness units by sitting next to Carol.
+      `,
+      330,
+    ],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part1(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part1(await data)).toMatchInlineSnapshot(`664`)
+  })
+})
+
+describe('Part 2', () => {
+  it.each([
+    [
+      `
+        Alice would gain 54 happiness units by sitting next to Bob.
+        Alice would lose 79 happiness units by sitting next to Carol.
+        Alice would lose 2 happiness units by sitting next to David.
+        Bob would gain 83 happiness units by sitting next to Alice.
+        Bob would lose 7 happiness units by sitting next to Carol.
+        Bob would lose 63 happiness units by sitting next to David.
+        Carol would lose 62 happiness units by sitting next to Alice.
+        Carol would gain 60 happiness units by sitting next to Bob.
+        Carol would gain 55 happiness units by sitting next to David.
+        David would gain 46 happiness units by sitting next to Alice.
+        David would lose 7 happiness units by sitting next to Bob.
+        David would gain 41 happiness units by sitting next to Carol.
+      `,
+      286,
+    ],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part2(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part2(await data)).toMatchInlineSnapshot(`640`)
+  })
+})
---
+++ src/2015/day-13/part-2.ts
@@ -0,0 +1,54 @@
+export default function (blob: string) {
+  let configuration = blob
+    .trim()
+    .split('\n')
+    .map((line) => line.trim())
+    .map(parse)
+
+  let people = new Set<string>(['me'])
+  let lookup: Record<string, Record<string, number>> = {}
+  for (let { lhs, rhs, amount } of configuration) {
+    lookup.me ??= {}
+    lookup.me[lhs] = 0
+    lookup.me[rhs] = 0
+    lookup[lhs] ??= {}
+    lookup[lhs].me = 0
+    lookup[lhs][rhs] = amount
+    people.add(lhs)
+    people.add(rhs)
+  }
+
+  let results: number[] = []
+  for (let arrangement of permutations(Array.from(people))) {
+    let sum = 0
+    for (let [idx, person] of arrangement.entries()) {
+      let lhs = arrangement[(idx + arrangement.length - 1) % arrangement.length]
+      sum += lookup[person][lhs]
+
+      let rhs = arrangement[(idx + arrangement.length + 1) % arrangement.length]
+      sum += lookup[person][rhs]
+    }
+
+    results.push(sum)
+  }
+
+  return results.reduce((a, b) => Math.max(a, b))
+}
+
+function parse(line: string) {
+  let parts = line.slice(0, -1).split(' ')
+  let lhs = parts.at(0)
+  let operand = parts.at(2)
+  let amount = Number(parts.at(3))
+  let rhs = parts.at(-1)
+
+  return { amount: operand === 'lose' ? -amount : amount, lhs, rhs }
+}
+
+function permutations<T>(input: T[]): T[][] {
+  if (input.length === 0) {
+    return [[]]
+  }
+
+  return input.reduce((rows, value, i) => [...rows, ...permutations([...input.slice(0, i), ...input.slice(i + 1)]).map((x) => [value, ...x])], [])
+}
