317613b14e7edc1d6e1ec2c49e6fb846ef206cee
Robin Malfait <malfait.robin@gmail.com>
2023-11-11 23:03:23 +0100

    add 2016 — day 06

---
+++ src/2016/day-06/index.test.ts
@@ -0,0 +1,71 @@
+import { promises } from 'fs'
+import { resolve } from 'path'
+
+import part1 from './part-1'
+import part2 from './part-2'
+
+let data = promises.readFile(resolve(process.cwd(), 'data', '2016-06.txt'), 'utf8')
+
+describe('Part 1', () => {
+  it.each([
+    [
+      `
+        eedadn
+        drvtee
+        eandsr
+        raavrd
+        atevrs
+        tsrnev
+        sdttsa
+        rasrtv
+        nssdts
+        ntnada
+        svetve
+        tesnvt
+        vntsnd
+        vrdear
+        dvrsen
+        enarar
+      `,
+      'easter',
+    ],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part1(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part1(await data)).toMatchInlineSnapshot(`"agmwzecr"`)
+  })
+})
+
+describe('Part 2', () => {
+  it.each([
+    [
+      `
+        eedadn
+        drvtee
+        eandsr
+        raavrd
+        atevrs
+        tsrnev
+        sdttsa
+        rasrtv
+        nssdts
+        ntnada
+        svetve
+        tesnvt
+        vntsnd
+        vrdear
+        dvrsen
+        enarar
+      `,
+      'advent',
+    ],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part2(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part2(await data)).toMatchInlineSnapshot(`"owlaxqvq"`)
+  })
+})
---
+++ src/2016/day-06/part-1.ts
@@ -0,0 +1,22 @@
+export default function (blob: string) {
+  return transpose(
+    blob
+      .trim()
+      .split('\n')
+      .map((line) => line.trim().split(''))
+  )
+    .map((characters) =>
+      characters
+        .sort()
+        .join('')
+        .match(/(.)\1*/g)
+        .sort((a, z) => z.length - a.length)
+        .at(0)
+        .at(0)
+    )
+    .join('')
+}
+
+function transpose<T>(matrix: T[][]) {
+  return matrix[0].map((_, i) => matrix.map((row) => row[i]))
+}
