4ced9b627ba7cfc13fc326ced6c21fc710fda97b
Robin Malfait <malfait.robin@gmail.com>
2023-11-15 20:23:20 +0100

    add 2017 — day 06 (part 2)

--- src/2017/day-06/part-2.js
+++ src/2017/day-06/part-2.js
@@ -5,11 +5,12 @@ function findIndexOfLargestNumber(data) {
 
 export default function (blob) {
   let memoryBanks = blob.trim().split('\t').map(Number)
-  let seen = new Set([])
+  let seen = new Map([])
+  let cycles = 0
 
   let next = memoryBanks.slice()
   while (!seen.has(next.join(','))) {
-    seen.add(next.join(','))
+    seen.set(next.join(','), cycles++)
 
     next = next.slice()
     let idx = findIndexOfLargestNumber(next)
@@ -22,5 +23,5 @@ export default function (blob) {
     }
   }
 
-  return seen.size
+  return cycles - seen.get(next.join(','))
 }
