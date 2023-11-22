d5161a191b96ab5626ed7c52e45ead5c8e6e1694
Robin Malfait <malfait.robin@gmail.com>
2023-11-23 00:30:43 +0100

    add 2018 — day 04

---
+++ src/2018/day-04/index.test.ts
@@ -0,0 +1,73 @@
+import { promises } from 'fs'
+import { resolve } from 'path'
+
+import part1 from './part-1'
+import part2 from './part-2'
+
+let data = promises.readFile(resolve(process.cwd(), 'data', '2018-04.txt'), 'utf8')
+
+describe('Part 1', () => {
+  it.each([
+    [
+      `
+        [1518-11-01 00:00] Guard #10 begins shift
+        [1518-11-01 00:05] falls asleep
+        [1518-11-01 00:25] wakes up
+        [1518-11-01 00:30] falls asleep
+        [1518-11-01 00:55] wakes up
+        [1518-11-01 23:58] Guard #99 begins shift
+        [1518-11-02 00:40] falls asleep
+        [1518-11-02 00:50] wakes up
+        [1518-11-03 00:05] Guard #10 begins shift
+        [1518-11-03 00:24] falls asleep
+        [1518-11-03 00:29] wakes up
+        [1518-11-04 00:02] Guard #99 begins shift
+        [1518-11-04 00:36] falls asleep
+        [1518-11-04 00:46] wakes up
+        [1518-11-05 00:03] Guard #99 begins shift
+        [1518-11-05 00:45] falls asleep
+        [1518-11-05 00:55] wakes up
+      `,
+      240,
+    ],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part1(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part1(await data)).toMatchInlineSnapshot(`140932`)
+  })
+})
+
+describe('Part 2', () => {
+  it.each([
+    [
+      `
+        [1518-11-01 00:00] Guard #10 begins shift
+        [1518-11-01 00:05] falls asleep
+        [1518-11-01 00:25] wakes up
+        [1518-11-01 00:30] falls asleep
+        [1518-11-01 00:55] wakes up
+        [1518-11-01 23:58] Guard #99 begins shift
+        [1518-11-02 00:40] falls asleep
+        [1518-11-02 00:50] wakes up
+        [1518-11-03 00:05] Guard #10 begins shift
+        [1518-11-03 00:24] falls asleep
+        [1518-11-03 00:29] wakes up
+        [1518-11-04 00:02] Guard #99 begins shift
+        [1518-11-04 00:36] falls asleep
+        [1518-11-04 00:46] wakes up
+        [1518-11-05 00:03] Guard #99 begins shift
+        [1518-11-05 00:45] falls asleep
+        [1518-11-05 00:55] wakes up
+      `,
+      4455,
+    ],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part2(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part2(await data)).toMatchInlineSnapshot(`51232`)
+  })
+})
---
+++ src/2018/day-04/part-1.ts
@@ -0,0 +1,84 @@
+import { compareAsc, differenceInMinutes } from 'date-fns'
+
+export default function (blob: string) {
+  let logs = blob
+    .trim()
+    .split('\n')
+    .map((line) => parse(line.trim()))
+    .sort((a, z) => compareAsc(a.date, z.date))
+
+  let totalSleepTime = new DefaultMap<number, number>(() => 0)
+  let minuteTracker = new DefaultMap<number, DefaultMap<number, number>>(() => new DefaultMap(() => 0))
+
+  let state = {
+    guard: null as null | number,
+    start: null as null | Date,
+  }
+
+  for (let log of logs) {
+    if (log.type === 'SHIFT') {
+      state.guard = log.id
+      state.start = null
+    } else if (log.type === 'SLEEP') {
+      state.start = log.date
+    } else if (log.type === 'WAKE') {
+      let end = log.date
+      let diff = differenceInMinutes(end, state.start)
+      totalSleepTime.set(state.guard, totalSleepTime.get(state.guard) + diff)
+      for (let i of Array(diff).keys()) {
+        let key = state.start.getMinutes() + i
+        minuteTracker.get(state.guard).set(key, minuteTracker.get(state.guard).get(key) + 1)
+      }
+    }
+  }
+
+  let chosenGuardId = Array.from(totalSleepTime.keys())
+    .sort((a, z) => totalSleepTime.get(a) - totalSleepTime.get(z))
+    .pop()
+
+  let chosenMinute = Array.from(minuteTracker.get(chosenGuardId).keys())
+    .sort((a, z) => minuteTracker.get(chosenGuardId).get(a) - minuteTracker.get(chosenGuardId).get(z))
+    .pop()
+
+  return chosenGuardId * chosenMinute
+}
+
+function parse(input: string) {
+  let { groups } = /\[(?<date>.*)\] (?<message>.*)/.exec(input)
+
+  if (groups.message === 'falls asleep') {
+    return {
+      type: 'SLEEP',
+      date: new Date(groups.date),
+    }
+  } else if (groups.message === 'wakes up') {
+    return {
+      type: 'WAKE',
+      date: new Date(groups.date),
+    }
+  } else {
+    let {
+      groups: { guardId },
+    } = /Guard #(?<guardId>\d+) begins shift/.exec(groups.message)
+
+    return {
+      type: 'SHIFT',
+      date: new Date(groups.date),
+      id: Number(guardId),
+    }
+  }
+}
+
+class DefaultMap<TKey = string, TValue = any> extends Map<TKey, TValue> {
+  constructor(private factory: (key: TKey) => TValue) {
+    super()
+  }
+
+  get(key: TKey) {
+    if (!this.has(key)) {
+      this.set(key, this.factory(key))
+    }
+
+    return super.get(key)
+  }
+}
