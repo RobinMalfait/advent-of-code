4706fa0a325e5b406df310448133ec4d31c231cb
Robin Malfait <malfait.robin@gmail.com>
2023-11-25 19:32:12 +0100

    add 2018 — day 10

---
+++ src/2018/day-10/part-1.ts
@@ -0,0 +1,64 @@
+export default function (blob: string) {
+  let points = blob
+    .trim()
+    .split('\n')
+    .map((line) => parse(line.trim()))
+
+  while (true) {
+    let minY = Infinity
+    let maxY = -Infinity
+
+    let xCount = {}
+
+    for (let point of points) {
+      point.tick()
+
+      xCount[point.x] ??= 0
+      xCount[point.x] += 1
+
+      minY = Math.min(minY, point.y)
+      maxY = Math.max(maxY, point.y)
+    }
+
+    if (
+      maxY - minY <= 10 && // All points are in the same area
+      Object.values(xCount).some((v) => v >= 7) // There should be a straight line
+    ) {
+      return print(points)
+    }
+  }
+}
+
+enum Pixels {
+  empty = '░',
+  filled = '█',
+}
+
+function print(points: Point[]) {
+  let minX = Math.min(...points.map((point) => point.x))
+  let maxX = Math.max(...points.map((point) => point.x))
+  let minY = Math.min(...points.map((point) => point.y))
+  let maxY = Math.max(...points.map((point) => point.y))
+  let grid = []
+  for (let y = minY; y <= maxY; y++) {
+    let row = []
+    for (let x = minX; x <= maxX; x++) {
+      row.push(points.some((point) => point.x === x && point.y === y) ? Pixels.filled : Pixels.empty)
+    }
+    grid.push(row.join(''))
+  }
+  return `\n${grid.join('\n')}\n`
+}
+
+function parse(input: string) {
+  let [, posX, posY, velX, velY] = /position=<([\s\d-]+),([\s\d-]+)> velocity=<([\s\d-]+),([\s\d-]+)>/.exec(input)
+  return new Point(Number(posX), Number(posY), Number(velX), Number(velY))
+}
+
+class Point {
+  constructor(public x: number, public y: number, private velocityX: number, private velocityY: number) {}
+  tick() {
+    this.x += this.velocityX
+    this.y += this.velocityY
+  }
+}
---
+++ src/2018/day-10/part-2.ts
@@ -0,0 +1,45 @@
+export default function (blob: string) {
+  let points = blob
+    .trim()
+    .split('\n')
+    .map((line) => parse(line.trim()))
+
+  let seconds = 0
+  while (true) {
+    seconds += 1
+    let minY = Infinity
+    let maxY = -Infinity
+
+    let xCount = {}
+
+    for (let point of points) {
+      point.tick()
+
+      xCount[point.x] ??= 0
+      xCount[point.x] += 1
+
+      minY = Math.min(minY, point.y)
+      maxY = Math.max(maxY, point.y)
+    }
+
+    if (
+      maxY - minY <= 10 && // All points are in the same area
+      Object.values(xCount).some((v) => v >= 7) // There should be a straight line
+    ) {
+      return seconds
+    }
+  }
+}
+
+function parse(input: string) {
+  let [, posX, posY, velX, velY] = /position=<([\s\d-]+),([\s\d-]+)> velocity=<([\s\d-]+),([\s\d-]+)>/.exec(input)
+  return new Point(Number(posX), Number(posY), Number(velX), Number(velY))
+}
+
+class Point {
+  constructor(public x: number, public y: number, private velocityX: number, private velocityY: number) {}
+  tick() {
+    this.x += this.velocityX
+    this.y += this.velocityY
+  }
+}
