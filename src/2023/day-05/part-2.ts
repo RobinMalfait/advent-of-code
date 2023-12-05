fb368f1d52c47801f78c8999a46e578577a2d7a1
Robin Malfait <malfait.robin@gmail.com>
2023-12-06 00:24:06 +0100

    implement `locateRange`. ~20min -> ~2ms

--- src/2023/day-05/part-2.ts
+++ src/2023/day-05/part-2.ts
@@ -4,20 +4,106 @@ export default function (blob: string) {
   let min = Infinity
 
   for (let [start, len] of chunk(seeds, 2)) {
-    for (let seed = start; seed < start + len; seed++) {
-      let a = locate(seed, maps.get('seed-to-soil'))
-      let b = locate(a, maps.get('soil-to-fertilizer'))
-      let c = locate(b, maps.get('fertilizer-to-water'))
-      let d = locate(c, maps.get('water-to-light'))
-      let e = locate(d, maps.get('light-to-temperature'))
-      let f = locate(e, maps.get('temperature-to-humidity'))
-      let g = locate(f, maps.get('humidity-to-location'))
+    let q: [start: number, end: number][] = [
+      // Initial seed range
+      [start, start + len],
+    ]
 
-      min = Math.min(min, g)
+    let a = locateRange(q, maps.get('seed-to-soil'))
+    let b = locateRange(a, maps.get('soil-to-fertilizer'))
+    let c = locateRange(b, maps.get('fertilizer-to-water'))
+    let d = locateRange(c, maps.get('water-to-light'))
+    let e = locateRange(d, maps.get('light-to-temperature'))
+    let f = locateRange(e, maps.get('temperature-to-humidity'))
+    let g = locateRange(f, maps.get('humidity-to-location'))
+
+    for (let [start] of g) {
+      min = Math.min(min, start)
     }
   }
 
   return min
+}
+
+function locateRange(seedRanges: [start: number, end: number][], mapRanges: [dst: number, src: number, len: number][]) {
+  let done = []
+
+  for (let [dst, src, len] of mapRanges) {
+    let rangeStart = src
+    let rangeEnd = src + len
+
+    for (let [seedStart, seedEnd] of seedRanges.splice(0)) {
+      // Comparison chart:
+      // 0.          ├──────────────────────────┤              Range we are comparing with (rangeStart, rangeEnd)
+      //             ┊                          ┊
+      // 1. ├──────┤ ┊                          ┊              Completely before
+      //             ┊                          ┊
+      // 2.          ┊                          ┊ ├───────┤    Completely after
+      //             ┊                          ┊
+      // 3.        ├──────────────────────────────┤            Completely overlapping
+      //             ┊                          ┊
+      // 4.    ├───────────┤                    ┊              Overlapping start
+      //             ┊                          ┊
+      // 5.          ┊                    ├───────────┤        Overlapping end
+      //             ┊                          ┊
+      // 6.          ┊      ├────────────┤      ┊              Completely inside
+
+      // 🎶 Cut my ranges into pieces, this is my last resort. Suffication computing.
+
+      // 0.          ├──────────────────────────┤
+      // 1. ├──────┤ ┊                          ┊
+      if (seedEnd < rangeStart) {
+        seedRanges.push([seedStart, Math.min(seedEnd, rangeStart)])
+      }
+
+      // 0.          ├──────────────────────────┤
+      // 2.          ┊                          ┊ ├───────┤
+      else if (seedStart > rangeEnd) {
+        seedRanges.push([Math.max(rangeEnd, seedStart), seedEnd])
+      }
+
+      // 0.          ├──────────────────────────┤
+      // 3.        ├──────────────────────────────┤
+      else if (seedStart <= rangeStart && seedEnd >= rangeEnd) {
+        // Before part
+        seedRanges.push([seedStart, rangeStart])
+
+        // Middle part
+        done.push([dst, dst + (rangeEnd - rangeStart)])
+
+        // After part
+        seedRanges.push([rangeEnd, seedEnd])
+      }
+
+      // 0.          ├──────────────────────────┤
+      // 4.    ├───────────┤                    ┊
+      else if (seedStart < rangeStart && seedEnd > rangeStart) {
+        // Before part
+        seedRanges.push([seedStart, rangeStart])
+
+        // Middle part
+        done.push([dst, dst + (seedEnd - rangeStart)])
+      }
+
+      // 0.          ├──────────────────────────┤
+      // 5.          ┊                    ├───────────┤
+      else if (seedStart < rangeEnd && seedEnd > rangeEnd) {
+        // Middle part
+        done.push([dst + (seedStart - rangeStart), dst + (rangeEnd - rangeStart)])
+
+        // After part
+        seedRanges.push([rangeEnd, seedEnd])
+      }
+
+      // 0.          ├──────────────────────────┤
+      // 6.          ┊      ├────────────┤      ┊
+      else if (seedStart >= rangeStart && seedEnd <= rangeEnd) {
+        done.push([dst + (seedStart - rangeStart), dst + (seedEnd - rangeStart)])
+      }
+    }
+  }
+
+  return done.concat(seedRanges)
 }
 
 function chunk<T>(input: T[], size: number): T[][] {
@@ -26,15 +112,6 @@ function chunk<T>(input: T[], size: number): T[][] {
     output.push(input.slice(i, i + size))
   }
   return output
-}
-
-function locate(input: number, ranges: [dst: number, src: number, len: number][]): number {
-  for (let [dst, src, len] of ranges) {
-    if (input >= src && input < src + len) {
-      return dst + (input - src)
-    }
-  }
-  return input
 }
 
 function parse(input: string) {
