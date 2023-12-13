599bd2216cc7a80cf6fa2178c1143a9ee030f822
Robin Malfait <malfait.robin@gmail.com>
2023-12-13 20:20:52 +0100

    add 2023 — day 13

---
+++ src/2023/day-13/index.test.ts
@@ -0,0 +1,69 @@
+import { promises } from 'fs'
+import { resolve } from 'path'
+
+import part1 from './part-1'
+import part2 from './part-2'
+
+let data = promises.readFile(resolve(__dirname, '..', '..', '..', 'data', '2023-13.txt'), 'utf8')
+
+describe('Part 1', () => {
+  it.each([
+    [
+      `
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+      `,
+      405,
+    ],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part1(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part1(await data)).toMatchInlineSnapshot(`33520`)
+  })
+})
+
+describe('Part 2', () => {
+  it.each([
+    [
+      `
+        #.##..##.
+        ..#.##.#.
+        ##......#
+        ##......#
+        ..#.##.#.
+        ..##..##.
+        #.#.##.#.
+
+        #...##..#
+        #....#..#
+        ..##..###
+        #####.##.
+        #####.##.
+        ..##..###
+        #....#..#
+      `,
+      400,
+    ],
+  ])('should produce the correct value for example %#', (input, expected) => {
+    expect(part2(input)).toBe(expected)
+  })
+
+  it('should produce the correct value for the input data', async () => {
+    expect(part2(await data)).toMatchInlineSnapshot(`34824`)
+  })
+})
---
+++ src/2023/day-13/part-2.ts
@@ -0,0 +1,80 @@
+import { Point, bail, match, range, sum, transposePointMap } from 'aoc-utils'
+
+export default function (blob: string) {
+  return sum(
+    parse(blob.trim())
+      .map(split)
+      .map(([orientation, x]) => {
+        return match(orientation, {
+          [Split.Vertical]: x,
+          [Split.Horizontal]: x * 100,
+        })
+      })
+  )
+}
+
+enum Split {
+  Horizontal,
+  Vertical,
+}
+
+function split([grid, { width, height }]: [Map<Point, string>, { width: number; height: number }]) {
+  return (
+    // Horizontal
+    findSplitPoint(grid, width, height, Split.Horizontal) ||
+    // Vertical
+    findSplitPoint(transposePointMap(grid), height, width, Split.Vertical) ||
+    // Sad
+    bail('Could not find a split point. Big sad.')
+  )
+}
+
+function findSplitPoint(pattern: Map<Point, string>, width: number, height: number, orientation: Split) {
+  next: for (let y = 1; y < height; y++) {
+    let r = Math.min(y, height - y)
+    let ys = Array.from(range(0, r)).map((i) => [y - i - 1, y + i])
+    let smudges = 0
+
+    for (let x = 0; x < width; x++) {
+      for (let [y1, y2] of ys) {
+        if (pattern.get(Point.new(x, y1)) !== pattern.get(Point.new(x, y2))) {
+          if (smudges++ > 1) {
+            continue next
+          }
+        }
+      }
+    }
+
+    if (smudges === 1) {
+      return [orientation, y] as const
+    }
+  }
+}
+
+function parse(input: string) {
+  return input.split('\n\n').map(parsePatterns)
+}
+
+function parsePatterns(input: string) {
+  let width = 0
+  let height = 0
+
+  let points = new Map(
+    input
+      .trim()
+      .split('\n')
+      .flatMap((line, y) =>
+        line
+          .trim()
+          .split('')
+          .flatMap((char, x) => {
+            width = Math.max(width, x + 1)
+            height = Math.max(height, y + 1)
+
+            return [[Point.new(x, y), char]]
+          })
+      )
+  )
+
+  return [points, { width, height }] as const
+}
