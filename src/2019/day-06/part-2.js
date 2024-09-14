ff5e949520f28c512fc5e377415492c4fb0d315f
Robin Malfait <malfait.robin@gmail.com>
2024-09-14 16:56:10 +0200

    use for of loop

--- src/2019/day-06/part-2.js
+++ src/2019/day-06/part-2.js
@@ -3,14 +3,14 @@
 module.exports = function calculateOrbitalTransfers(input, start, target) {
   const graph = new Graph()
 
-  input.split('\n').forEach((value) => {
-    const [k, v] = value.split(')')
+  for (let value of input.split('\n')) {
+    let [k, v] = value.split(')')
 
     graph.addVertex(k)
     graph.addVertex(v)
 
     graph.addEdge(k, v)
-  })
+  }
 
   // -2: to exclude start & target
   // -1: because we want the edges, not the nodes
@@ -46,15 +46,15 @@ class Graph {
     while (queue.length) {
       const vertex = queue.shift()
 
-      this.edges[vertex].forEach((vertex_from_edge) => {
+      for (let vertex_from_edge of this.edges[vertex]) {
         if (visited.includes(vertex_from_edge)) {
-          return
+          continue
         }
 
         visited.push(vertex_from_edge)
         queue.push(vertex_from_edge)
         parent[vertex_from_edge] = vertex
-      })
+      }
     }
 
     if (!visited.includes(target)) {
