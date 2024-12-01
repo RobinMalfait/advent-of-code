bc81e5747cc49a0a8cd3e530393c8b5b500ba897
Robin Malfait <malfait.robin@gmail.com>
2024-12-01 11:39:44 +0100

    cleanup template

--- templates/day/bench.ts
+++ templates/day/bench.ts
@@ -5,9 +5,7 @@ import { bench, run } from 'mitata'
 import part1 from './part-1'
 import part2 from './part-2'
 
-let data = await Bun.file(
-  resolve(__dirname, '..', '..', '..', 'data', '{{YEAR}}-{{DAY}}.txt')
-).text()
+let data = await Bun.file(resolve(__dirname, '../../../data/{{YEAR}}-{{DAY}}.txt')).text()
 
 bench('Part 1', () => part1(data))
 bench('Part 2', () => part2(data))
--- templates/day/index.test.ts
+++ templates/day/index.test.ts
@@ -5,10 +5,7 @@ import { describe, expect, it } from 'vitest'
 import part1 from './part-1'
 import part2 from './part-2'
 
-let data = promises.readFile(
-  resolve(__dirname, '..', '..', '..', 'data', '{{YEAR}}-{{DAY}}.txt'),
-  'utf8'
-)
+let data = promises.readFile(resolve(__dirname, '../../../data/{{YEAR}}-{{DAY}}.txt'), 'utf8')
 
 describe('Part 1', () => {
   it.each([
