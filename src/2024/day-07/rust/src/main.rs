0c84e7985b3a4f38802331aa7b0e4cb5d31ac8bb
Robin Malfait <malfait.robin@gmail.com>
2024-12-07 19:54:34 +0100

    add 2024 — day 07 (Rust)

--- src/2024/day-07/rust/src/main.rs
+++ src/2024/day-07/rust/src/main.rs
@@ -14,12 +14,102 @@ fn main() {
     println!("Part 2: {:<20}(took: {:>12?})", part_2_result, duration);
 }
 
-pub fn part_1(data: &str) -> i32 {
-    todo!()
+pub fn part_1(blob: &str) -> i64 {
+    blob.trim()
+        .lines()
+        .map(|line| line.trim())
+        .map(|line| {
+            let Some((target, args)) = line.split_once(": ") else {
+                panic!("Invalid line: {}", line);
+            };
+            let target = target.parse::<i64>().expect("A target");
+            let args = args
+                .split(' ')
+                .map(|x| x.parse::<i64>().expect("A number"))
+                .collect::<Vec<_>>();
+            (target, args)
+        })
+        .map(|(target, args)| {
+            let mut results = vec![args[0]];
+
+            for arg in args.iter().skip(1) {
+                let copy = results.clone();
+                results.clear();
+                for result in copy {
+                    let add = result + arg;
+                    if add <= target {
+                        results.push(add);
+                    }
+
+                    let mul = result * arg;
+                    if mul <= target {
+                        results.push(mul);
+                    }
+                }
+            }
+
+            if results.contains(&target) {
+                target
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
-pub fn part_2(data: &str) -> i32 {
-    todo!()
+pub fn part_2(blob: &str) -> i64 {
+    blob.trim()
+        .lines()
+        .map(|line| line.trim())
+        .map(|line| {
+            let Some((target, args)) = line.split_once(": ") else {
+                panic!("Invalid line: {}", line);
+            };
+            let target = target.parse::<i64>().expect("A target");
+            let args = args
+                .split(' ')
+                .map(|x| x.parse::<i64>().expect("A number"))
+                .collect::<Vec<_>>();
+            (target, args)
+        })
+        .map(|(target, args)| {
+            let mut results = vec![args[0]];
+
+            for arg in args.iter().skip(1) {
+                let copy = results.clone();
+                results.clear();
+                for result in copy {
+                    let add = result + arg;
+                    if add <= target {
+                        results.push(add);
+                    }
+
+                    let mul = result * arg;
+                    if mul <= target {
+                        results.push(mul);
+                    }
+
+                    let concat = match arg {
+                        c if *c < 10 => result * 10 + arg,
+                        c if *c < 100 => result * 100 + arg,
+                        c if *c < 1000 => result * 1000 + arg,
+                        _ => format!("{}{}", result, arg)
+                            .parse::<i64>()
+                            .expect("A number"),
+                    };
+                    if concat <= target {
+                        results.push(concat);
+                    }
+                }
+            }
+
+            if results.contains(&target) {
+                target
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -29,29 +119,44 @@ mod tests {
     #[test]
     fn part_1_sample() {
         let data = r#"
+            190: 10 19
+            3267: 81 40 27
+            83: 17 5
+            156: 15 6
+            7290: 6 8 6 15
+            161011: 16 10 13
+            192: 17 8 14
+            21037: 9 7 18 13
+            292: 11 6 16 20
         "#;
 
-        assert_eq!(part_1(data), 0);
+        assert_eq!(part_1(data), 3749);
     }
 
     #[test]
-    #[ignore]
     fn part_1_real() {
-        assert_eq!(part_1(INPUT), 0);
+        assert_eq!(part_1(INPUT), 6392012777720);
     }
 
     #[test]
-    #[ignore]
     fn part_2_sample() {
         let data = r#"
+            190: 10 19
+            3267: 81 40 27
+            83: 17 5
+            156: 15 6
+            7290: 6 8 6 15
+            161011: 16 10 13
+            192: 17 8 14
+            21037: 9 7 18 13
+            292: 11 6 16 20
         "#;
 
-        assert_eq!(part_2(data), 0);
+        assert_eq!(part_2(data), 11387);
     }
 
     #[test]
-    #[ignore]
     fn part_2_real() {
-        assert_eq!(part_2(INPUT), 0);
+        assert_eq!(part_2(INPUT), 61561126043536);
     }
 }
