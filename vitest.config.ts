9c8ebb2fe217b55270387bdb59b68ebc8b7dfef7
Robin Malfait <malfait.robin@gmail.com>
2025-11-30 17:43:56 +0100

    update vitest configuration

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+export default defineConfig({
+  test: {
+    // Each year lives in its own directory in the src folder. Since I'm not
+    // planning to live until the year 3000, it's safe to assume that the years
+    // will start with 2.
+    //
+    // If you are reading this from the future, I'm sorry for the inconvenience.
+    projects: ['src/2*'],
+
+    // 5 minutes
+    testTimeout: 5 * 60 * 1000,
+  },
+})
