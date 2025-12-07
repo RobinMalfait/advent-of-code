d32c69659fd3153df6676d71285a6fa778f2599b
Robin Malfait <malfait.robin@gmail.com>
2025-12-07 13:59:01 +0100

    optimization & simplification: use DP
    Had to use a recursive DefaultMap, right?! Would it really be a Robin
    solution without a DefaultMap? Ok fine, used a DefaultMap for the
    counters before but we can cache the result since `Point`s are stable 🧘‍♂️

--- src/2025/day-07/part-2.ts
+++ src/2025/day-07/part-2.ts
@@ -1,4 +1,4 @@
-import { DefaultMap, parseIntoGrid, uniqueQueue, type Point } from 'aoc-utils'
+import { DefaultMap, parseIntoGrid, type Point } from 'aoc-utils'
 
 export default function (blob: string) {
   let start: Point
@@ -7,43 +7,19 @@ export default function (blob: string) {
     return x
   })
 
-  let counter = new DefaultMap((_point: Point) => ({ count: 0 }))
-  counter.get(start).count = 1
+  let steps = new DefaultMap((point: Point) => {
+    let next = point.down()
 
-  let total = 0
-  let q = uniqueQueue<Point>([start])
-  for (let current of q) {
-    let steps = counter.get(current).count
-
-    if (current.y === grid.height - 1) {
-      total += steps
-      continue
+    if (next.y === grid.height - 1) {
+      return 1
     }
 
-    let next = current.down()
-    switch (grid.get(next)) {
-      case '.': {
-        counter.get(next).count += steps
-        q.push(next)
-        break
-      }
-
-      case '^': {
-        let left = next.left()
-        if (grid.get(left) === '.') {
-          counter.get(left).count += steps
-          q.push(left)
-        }
-
-        let right = next.right()
-        if (grid.get(right) === '.') {
-          counter.get(right).count += steps
-          q.push(right)
-        }
-        break
-      }
+    if (grid.get(next) === '^') {
+      return steps.get(next.left()) + steps.get(next.right())
     }
-  }
 
-  return total
+    return steps.get(next)
+  })
+
+  return steps.get(start)
 }
